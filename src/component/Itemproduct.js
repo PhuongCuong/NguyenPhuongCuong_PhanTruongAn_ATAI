47f2dd2b333c070b3fd756d8800a4147d2bf1d99
PhuonCuong <you@example.com>
2023-11-04 09:42:16 +0700

    upload fix img

--- src/component/Itemproduct.js
+++ src/component/Itemproduct.js
@@ -17,7 +17,9 @@ const Itemproduct = (props) => {
             },
             overflow: "hidden"
         }}>
-            <Image style={{ width: 69, height: 54 }} source={item.img} />
+            <View style={{ width: 69, height: 54 }}>
+                <Image style={{ flex: 1, height: undefined, width: undefined, resizeMode: "contain" }} source={item.img} />
+            </View>
             <Text style={{
                 width: 70, height: 25, color: "#000",
                 fontFamily: "Inter", fontSize: 12, fontWeight: "400", fontStyle: "normal", textAlign: "center", marginTop: 5
