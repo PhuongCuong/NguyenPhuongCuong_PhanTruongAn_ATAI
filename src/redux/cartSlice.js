4eeb5d98228757ea1d4a9c2e8aa6af626fba1082
PhanTruongAn <phanan1000@gmail.com>
2023-11-26 20:05:07 +0700

    Fix Cart Bug

--- src/redux/cartSlice.js
+++ src/redux/cartSlice.js
@@ -1,19 +1,34 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 const initialState = {
-    cart: []
-}
+  cart: [],
+};
 
-export const counterSlice = createSlice({
-    name: "cartinfo",
-    initialState,
-    reducers: {
-        uploadcart: (state, action) => {
-            state.cart = action.payload
+export const cartDelete = createAsyncThunk(
+  "cartinfo/constDelete",
+  async (item) => {
+    const response = await fetch(
+      `https://65434e0301b5e279de202812.mockapi.io/User/${item.id}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
         },
-
-    }
-})
+        body: JSON.stringify({ ...item, cart: [...item.cart] }),
+      }
+    );
+    return await response.json();
+  }
+);
+export const counterSlice = createSlice({
+  name: "cartinfo",
+  initialState,
+  reducers: {
+    uploadcart: (state, action) => {
+      state.cart = action.payload;
+    },
+  },
+});
 
 export const { uploadcart } = counterSlice.actions;
 
